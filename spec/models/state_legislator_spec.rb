b4a4223db0fe0f611bb2c722537933ef77eb6ad2
Dan McKeon <email": null>
2017-05-17 16:58:31 -0700

    write additional instantiation test and 1 association test

--- spec/models/state_legislator_spec.rb
+++ spec/models/state_legislator_spec.rb
@@ -4,9 +4,25 @@ RSpec.describe StateLegislator, type: :model do
   let(:state_legislator) { FactoryGirl.build(:state_legislator) }
   let(:state_district) { FactoryGirl.create(:state_district) }
 
-  it 'persists in database' do
-    state_legislator.state_district = state_district
-    state_legislator.save
-    expect(state_legislator.created_at).not_to be_nil
+  describe 'instantiation' do
+    it 'persists in database' do
+      state_legislator.state_district = state_district
+      state_legislator.save
+      expect(state_legislator.created_at).not_to be_nil
+    end
+
+    it 'saves name correctly' do
+      state_legislator.state_district = state_district
+      state_legislator.save
+      expect(state_legislator.full_name).to eq "Dan McKeon"
+    end
+  end
+
+  describe 'associations' do
+    it 'belongs to a state district' do
+      state_legislator.state_district = state_district
+      state_legislator.save
+      expect(state_legislator.state_district).to eq state_district
+    end
   end
 end
