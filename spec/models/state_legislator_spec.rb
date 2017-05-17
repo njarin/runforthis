abeaef11a1f09bc56fbba852d4f2b0188e3a3ab9
Dan McKeon <email": null>
2017-05-17 16:11:46 -0700

    add sample test to StateLegislator model spec

--- spec/models/state_legislator_spec.rb
+++ spec/models/state_legislator_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe StateLegislator, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'test to ensure rspec is correctly configured' do
+    dan = StateLegislator.create(full_name: "Dan McKeon", title: "Representative", party: "D", website: "www.danmckeon.com", term_end: "2018")
+    expect(dan.full_name).to eq "Dan McKeon"
+  end
 end
