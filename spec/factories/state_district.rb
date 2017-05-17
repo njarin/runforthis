8bcfaf405845e80b7dabbc5db8c07637f431c6c3
Dan McKeon <email": null>
2017-05-17 16:48:12 -0700

    add state_district factory and write test for state legislator persistence in db

---
+++ spec/factories/state_district.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :state_district do
+    state "WA"
+    name "Congressional District 7"
+  end
+end
--- spec/models/state_legislator_spec.rb
+++ spec/models/state_legislator_spec.rb
@@ -1,8 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe StateLegislator, type: :model do
-  let(:state_legislator) { FactoryGirl.create(:state_legislator) }
-  it 'test to ensure rspec is correctly configured' do
-    expect(state_legislator.full_name).to eq "Dan McKeon"
+  let(:state_legislator) { FactoryGirl.build(:state_legislator) }
+  let(:state_district) { FactoryGirl.create(:state_district) }
+
+  it 'persists in database' do
+    state_legislator.state_district = state_district
+    state_legislator.save
+    expect(state_legislator.created_at).not_to be_nil
   end
 end
