bda22ee4ba92c378a6519f8f63e6c256b293781d
Dan McKeon <email": null>
2017-05-18 15:04:03 -0700

    add resources to each district

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,49 +5,51 @@ Resource.destroy_all
 WA_STATE_REPS_LINK = 'http://leg.wa.gov/house/representatives/pages/default.aspx'
 WA_STATE_SENS_LINK = 'http://leg.wa.gov/senate/senators/pages/default.aspx'
 
-Resource.create(name: "Washington State Democratic Party", link: "http://www.wa-democrats.org/")
-Resource.create(name: "Washington State Republican Party", link: "https://wsrp.org/")
+dem_resource = Resource.create(name: "Washington State Democratic Party", link: "http://www.wa-democrats.org/")
+rep_resource = Resource.create(name: "Washington State Republican Party", link: "https://wsrp.org/")
 
 49.times do |i|
-  StateDistrict.create(state: "WA", name: "State House District #{i + 1}")
-  StateDistrict.create(state: "WA", name: "State Senate District #{i + 1}")
+  state_house_district = StateDistrict.create(state: "WA", name: "State House District #{i + 1}")
+  state_house_district.resources << dem_resource << rep_resource
+  state_senate_district = StateDistrict.create(state: "WA", name: "State Senate District #{i + 1}")
+  state_senate_district.resources << dem_resource << rep_resource
 end
 
 
 
 
 
-wa_state_reps_noko = Nokogiri::HTML(open(WA_STATE_REPS_LINK))
-
-wa_state_reps_noko.css('.memberInformation').each do |member|
-  state_legislator = StateLegislator.new
-  member_name_raw_arr = member.at('.memberName').text.split(' ')
-  state_legislator.full_name = member_name_raw_arr[1..-2].join(' ')
-  state_legislator.title = member_name_raw_arr[0] == 'Representative' ? 'Rep.' : 'Sen.'
-  state_legislator.party = member_name_raw_arr[-1].gsub(/[()]/, '')
-  state_legislator.website = member.at_css('a:contains("Home Page")')["href"]
-  state_legislator.term_end = "November 2018"
-  district_name_raw = member.at_css('a:contains("Legislative District")').text
-  district_num = district_name_raw[/\d+/]
-  district_name = "State House District #{district_num}"
-  state_legislator.state_district = StateDistrict.find_by(name: district_name)
-  state_legislator.save
-end
-
-wa_state_sens_noko = Nokogiri::HTML(open(WA_STATE_SENS_LINK))
-
-wa_state_sens_noko.css('.memberInformation').each do |member|
-  state_legislator = StateLegislator.new
-  member_name_raw_arr = member.at('.memberName').text.split(' ')
-  state_legislator.full_name = member_name_raw_arr[1..-2].join(' ')
-  state_legislator.title = member_name_raw_arr[0] == 'Representative' ? 'Rep.' : 'Sen.'
-  state_legislator.party = member_name_raw_arr[-1].gsub(/[()]/, '')
-  state_legislator.website = member.at_css('a:contains("Home Page")')["href"]
-  # NEED TO FIX SENATOR TERMS!
-  state_legislator.term_end = "November 2018"
-  district_name_raw = member.at_css('a:contains("Legislative District")').text
-  district_num = district_name_raw[/\d+/]
-  district_name = "State Senate District #{district_num}"
-  state_legislator.state_district = StateDistrict.find_by(name: district_name)
-  state_legislator.save
-end
+# wa_state_reps_noko = Nokogiri::HTML(open(WA_STATE_REPS_LINK))
+#
+# wa_state_reps_noko.css('.memberInformation').each do |member|
+#   state_legislator = StateLegislator.new
+#   member_name_raw_arr = member.at('.memberName').text.split(' ')
+#   state_legislator.full_name = member_name_raw_arr[1..-2].join(' ')
+#   state_legislator.title = member_name_raw_arr[0] == 'Representative' ? 'Rep.' : 'Sen.'
+#   state_legislator.party = member_name_raw_arr[-1].gsub(/[()]/, '')
+#   state_legislator.website = member.at_css('a:contains("Home Page")')["href"]
+#   state_legislator.term_end = "November 2018"
+#   district_name_raw = member.at_css('a:contains("Legislative District")').text
+#   district_num = district_name_raw[/\d+/]
+#   district_name = "State House District #{district_num}"
+#   state_legislator.state_district = StateDistrict.find_by(name: district_name)
+#   state_legislator.save
+# end
+#
+# wa_state_sens_noko = Nokogiri::HTML(open(WA_STATE_SENS_LINK))
+#
+# wa_state_sens_noko.css('.memberInformation').each do |member|
+#   state_legislator = StateLegislator.new
+#   member_name_raw_arr = member.at('.memberName').text.split(' ')
+#   state_legislator.full_name = member_name_raw_arr[1..-2].join(' ')
+#   state_legislator.title = member_name_raw_arr[0] == 'Representative' ? 'Rep.' : 'Sen.'
+#   state_legislator.party = member_name_raw_arr[-1].gsub(/[()]/, '')
+#   state_legislator.website = member.at_css('a:contains("Home Page")')["href"]
+#   # NEED TO FIX SENATOR TERMS!
+#   state_legislator.term_end = "November 2018"
+#   district_name_raw = member.at_css('a:contains("Legislative District")').text
+#   district_num = district_name_raw[/\d+/]
+#   district_name = "State Senate District #{district_num}"
+#   state_legislator.state_district = StateDistrict.find_by(name: district_name)
+#   state_legislator.save
+# end
