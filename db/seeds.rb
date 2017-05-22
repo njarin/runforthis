b087b3466b08856c0345f3e55802a0fea32123f0
Dan McKeon <email": null>
2017-05-22 16:46:50 -0700

    refactor create_state_legislators to only take one argument and assign variables conditionally

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,61 +1,72 @@
-wa_wiki_noko = Nokogiri::HTML(open("https://en.wikipedia.org/wiki/Washington_State_Senate"))
-wa_senate_table_noko = wa_wiki_noko.at_css('table:contains("Counties Represented")')
 
-wa_senate_term_ends = {}
 
-wa_senate_table_noko.css('tr').each do |member|
-  district = member.css('td').first.text if member.css('td').first
-  term_end_year = member.css('td').last.text if member.css('td').last
-  case term_end_year
-  when "2017"
-    term_end_date = "November 2017"
-  when "2018"
-    term_end_date = "November 2018"
-  when "2020"
-    term_end_date = "November 2020"
+
+StateLegislator.destroy_all
+StateDistrict.destroy_all
+Resource.destroy_all
+
+def create_state_legislators(wa_leg_noko)
+  wa_leg_noko.css('.memberInformation').each do |member|
+    state_legislator = StateLegislator.new
+    member_name_raw_arr = member.at('.memberName').text.split(' ')
+    state_legislator.full_name = member_name_raw_arr[1..-2].join(' ')
+    state_legislator.party = member_name_raw_arr[-1].gsub(/[()]/, '')
+    state_legislator.website = member.at_css('a:contains("Home Page")')["href"]
+    # NEED TO FIX SENATOR TERMS!
+    district_name_raw = member.at_css('a:contains("Legislative District")').text
+    district_num = district_name_raw[/\d+/]
+    if member_name_raw_arr[0] == 'Representative'
+      state_legislator.title =  'Rep.'
+      state_legislator.term_end = "November 2018"
+      district_name = "State House District #{district_num}"
+    else
+      state_legislator.title =  'Sen.'
+      state_legislator.term_end = "November 2018" #FIX THIS!!!
+      district_name = "State Senate District #{district_num}"
+    end
+    state_legislator.state_district = StateDistrict.find_by(name: district_name)
+    state_legislator.save
   end
-  wa_senate_term_ends[district] = term_end_date
 end
 
-p wa_senate_term_ends.length
+def find_senate_terms
+  wa_wiki_noko = Nokogiri::HTML(open('https://en.wikipedia.org/wiki/Washington_State_Senate'))
+  wa_senate_table_noko = wa_wiki_noko.at_css('table:contains("Counties Represented")')
 
-# StateLegislator.destroy_all
-# StateDistrict.destroy_all
-# Resource.destroy_all
+  wa_senate_term_ends = {}
 
-# def create_state_legislators(wa_leg_noko, body)
-#   wa_leg_noko.css('.memberInformation').each do |member|
-#     state_legislator = StateLegislator.new
-#     member_name_raw_arr = member.at('.memberName').text.split(' ')
-#     state_legislator.full_name = member_name_raw_arr[1..-2].join(' ')
-#     state_legislator.title = member_name_raw_arr[0] == 'Representative' ? 'Rep.' : 'Sen.'
-#     state_legislator.party = member_name_raw_arr[-1].gsub(/[()]/, '')
-#     state_legislator.website = member.at_css('a:contains("Home Page")')["href"]
-#     # NEED TO FIX SENATOR TERMS!
-#     state_legislator.term_end = "November 2018"
-#     district_name_raw = member.at_css('a:contains("Legislative District")').text
-#     district_num = district_name_raw[/\d+/]
-#     district_name = "State #{body} District #{district_num}"
-#     state_legislator.state_district = StateDistrict.find_by(name: district_name)
-#     state_legislator.save
-#   end
-# end
+  wa_senate_table_noko.css('tr').each do |member|
+    district = member.css('td').first.text if member.css('td').first
+    term_end_year = member.css('td').last.text if member.css('td').last
+    case term_end_year
+    when "2017"
+      term_end_date = "November 2017"
+    when "2018"
+      term_end_date = "November 2018"
+    when "2020"
+      term_end_date = "November 2020"
+    end
+    wa_senate_term_ends[district] = term_end_date
+  end
 
-# dem_resource = Resource.create(name: "Washington State Democratic Party", link: "http://www.wa-democrats.org/")
-# rep_resource = Resource.create(name: "Washington State Republican Party", link: "https://wsrp.org/")
+  wa_senate_term_ends
+end
 
-# 49.times do |i|
-#   state_house_district = StateDistrict.create(state: "WA", name: "State House District #{i + 1}")
-#   state_house_district.resources << dem_resource << rep_resource
-#   state_senate_district = StateDistrict.create(state: "WA", name: "State Senate District #{i + 1}")
-#   state_senate_district.resources << dem_resource << rep_resource
-# end
+dem_resource = Resource.create(name: "Washington State Democratic Party", link: "http://www.wa-democrats.org/")
+rep_resource = Resource.create(name: "Washington State Republican Party", link: "https://wsrp.org/")
 
-# wa_state_reps_link = 'http://leg.wa.gov/house/representatives/pages/default.aspx'
-# wa_state_sens_link = 'http://leg.wa.gov/senate/senators/pages/default.aspx'
+49.times do |i|
+  state_house_district = StateDistrict.create(state: "WA", name: "State House District #{i + 1}")
+  state_house_district.resources << dem_resource << rep_resource
+  state_senate_district = StateDistrict.create(state: "WA", name: "State Senate District #{i + 1}")
+  state_senate_district.resources << dem_resource << rep_resource
+end
 
-# wa_state_reps_noko = Nokogiri::HTML(open(wa_state_reps_link))
-# wa_state_sens_noko = Nokogiri::HTML(open(wa_state_sens_link))
+wa_state_reps_link = 'http://leg.wa.gov/house/representatives/pages/default.aspx'
+wa_state_sens_link = 'http://leg.wa.gov/senate/senators/pages/default.aspx'
 
-# create_state_legislators(wa_state_reps_noko, "House")
-# create_state_legislators(wa_state_sens_noko, "Senate")
+wa_state_reps_noko = Nokogiri::HTML(open(wa_state_reps_link))
+wa_state_sens_noko = Nokogiri::HTML(open(wa_state_sens_link))
+
+create_state_legislators(wa_state_reps_noko)
+create_state_legislators(wa_state_sens_noko)
