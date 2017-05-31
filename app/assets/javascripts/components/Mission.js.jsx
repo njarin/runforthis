45675eaef41045de92b3d56800bf36962ccd42bb
Nicholas Jarin <nicholasjarin@gmail.com>
2017-05-31 11:28:54 -0700

    Add constructor to Search

--- app/assets/javascripts/components/Mission.js.jsx
+++ app/assets/javascripts/components/Mission.js.jsx
@@ -5,7 +5,7 @@ class Mission extends React.Component {
       <div className="mission-component container">
         <div className="mission-statement-wrapper">
           <p className="mission-statement">
-            You should run for office! Local elected officials have a significant amount of impact on your day-to-day life. About 33% of state-level legislators run unopposed. That’s a problem if you believe in democracy. Enter your address below and see what local offices are opening up near you and how you can run for those seats.
+            You should run for office! No, really! Local elected officials have a significant impact on your day-to-day life. About 33% of state-level legislators run unopposed. That’s a problem if you believe in democracy. Enter your address below and see what local offices are opening up near you and how you can run for those seats.
           </p>
         </div>
         <div className="quote">
--- app/assets/javascripts/components/Search.js.jsx
+++ app/assets/javascripts/components/Search.js.jsx
@@ -1,11 +1,23 @@
 class Search extends React.Component {
 
+  constructor(){
+    super();
+    this.state = {
+      mode: fresh
+    }
+  }
+
+  clickHandler(e) {
+      
+  }
+
   render() {
     return (
       <div className="search-page">
         <NavBar />
         <Mission />
-        <SearchForm />
+        {/* <SearchForm /> */}
+        <div className="search-form-target" />
       </div>
     )
   }
