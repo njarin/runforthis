969da3343cef09452e1ccd275a0a056985892fad
Nicholas Jarin <nicholasjarin@gmail.com>
2017-05-31 11:42:12 -0700

    Remove button for toggling form

--- app/assets/javascripts/components/Mission.js.jsx
+++ app/assets/javascripts/components/Mission.js.jsx
@@ -1,10 +1,4 @@
 class Mission extends React.Component {
-  onClick(e) {
-    e.preventDefault();
-    console.log("We Made it!")
-    this.props.onClick(e)
-  }
-
   render() {
     return (
       <div className="mission-component container">
@@ -13,12 +7,11 @@ class Mission extends React.Component {
             You should run for office! No, really! Local elected officials have a significant impact on your day-to-day life. About 33% of state-level legislators run unopposed. That’s a problem if you believe in democracy. Enter your address below and see what local offices are opening up near you and how you can run for those seats.
           </p>
         </div>
-        <div className="quote">
-          <p className="mission-quote">
+        <div className="mission-quote">
+          <p className="quote">
             "If not us, who? If not now, when?"
           </p>
         </div>
-        <button onClick={(e) => this.onClick(e)}>Learn More</button>
       </div>
     )
   }
--- app/assets/javascripts/components/Search.js.jsx
+++ app/assets/javascripts/components/Search.js.jsx
@@ -1,23 +1,11 @@
 class Search extends React.Component {
 
-  constructor(){
-    super();
-    this.state = {
-      // mode: fresh
-    }
-  }
-
-  clickHandler(e) {
-    console.log(e);
-  }
-
   render() {
     return (
       <div className="search-page">
         <NavBar />
-        <Mission onClick={(e) => this.clickHandler(e)}/>
-        {/* <SearchForm /> */}
-        <div className="search-form-target" />
+        <Mission />
+        <SearchForm />
       </div>
     )
   }
