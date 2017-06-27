dce2e9952436a0e62c863050a4771dcb4e44b797
Nicholas Jarin <nicholasjarin@gmail.com>
2017-06-26 22:14:07 -0700

    Set up controller to handle about page

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,4 +2,8 @@ class SearchController < ApplicationController
   def search
     render 'search/search'
   end
+  def about
+    "Yoooooooooooooooooooo"
+    # render 'views/about'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'search#search'
   get '/results', to: 'results#results'
-  get '/about', to: 'about'
+  get '/about', to: 'search#about'
 end
