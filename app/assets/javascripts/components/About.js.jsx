91e11af6bfaefaa7b17404885477def2c259db83
Nicholas Jarin <nicholasjarin@gmail.com>
2017-06-26 22:17:14 -0700

    Set up React component for about page

---
+++ app/assets/javascripts/components/About.js.jsx
@@ -0,0 +1,18 @@
+class About extends React.Component {
+
+  render() {
+    return (
+      <div className="about-page">
+        <NavBar />
+        <div className="about-us-section">
+          <div className="about-nick">
+            
+          </div>
+          <div className="about-dan">
+
+          </div>
+        </div>
+      </div>
+    )
+  }
+};
