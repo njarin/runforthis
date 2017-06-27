8be5af54f79882889eeabd7e647cf901c00eac44
Nicholas Jarin <nicholasjarin@gmail.com>
2017-06-26 22:47:07 -0700

    Add content and start styling

--- app/assets/javascripts/components/About.js.jsx
+++ app/assets/javascripts/components/About.js.jsx
@@ -5,11 +5,11 @@ class About extends React.Component {
       <div className="about-page">
         <NavBar />
         <div className="about-us-section">
-          <div className="about-nick">
-            
+          <div className="about-nick column">
+            Nick Jarin is a web developer with a passion for politics and storytelling. When he isn't coding, you'll find him working on the <a href="http://nw-nerd.com/" target="_blank">NW NERD</a> podcast.
           </div>
-          <div className="about-dan">
-
+          <div className="about-dan column">
+            Dan McKeon uses his web developer skills for good, including his other projects like <a href="http://www.crconnect.org/" target="_blank">Cancer Research Connect</a>. When he isn't behind a keyboard, he prefers to be up on a mountain.
           </div>
         </div>
       </div>
