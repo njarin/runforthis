0dfa8179424404ac63b900123c153824bfcef2fa
Dan McKeon <dpmckeon@gmail.com>
2017-07-07 16:48:48 -0700

    format footer

--- app/assets/javascripts/components/Footer.js.jsx
+++ app/assets/javascripts/components/Footer.js.jsx
@@ -3,18 +3,18 @@ class Footer extends React.Component {
   render() {
     return (
       <div className="footer">
-        <span><a href="/about">About Us</a></span> | <span>Find our project on <a
-          href="https://github.com/njarin/runforthis" target="_blank">Github</a></span>
+        <p><a href="/about">About Us</a> | Find our project on <a
+          href="https://github.com/njarin/runforthis" target="_blank">Github</a></p>
         <p>Disclaimer: We have made our best effort to provide accurate data on this site,
           but cannot guarantee the validity due to frequent special elections and other
           factors that effect the timeliness of our data</p>
-        <div>
+        <p>
           Icons made by <a href="http://www.flaticon.com/authors/dave-gandy"
           title="Dave Gandy" target="_blank">Dave Gandy</a> from <a href="http://www.flaticon.com"
           title="Flaticon" target="_blank">www.flaticon.com</a> is licensed by <a
           href="http://creativecommons.org/licenses/by/3.0/"
           title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
-        </div>
+        </p>
       </div>
     )
   }
