208cecd94f595f90c693470c8be6af0c14f62492
Dan McKeon <dpmckeon@gmail.com>
2017-07-07 11:05:33 -0700

    add text content to footer

---
+++ app/assets/javascripts/components/Footer.js.jsx
@@ -0,0 +1,17 @@
+class Footer extends React.Component {
+
+  render() {
+    return (
+      <div className="footer">
+        <span><a href="/about">About Us</a></span> | <span>Find our project on <a href="https://github.com/njarin/runforthis" target="_blank">Github</a></span>
+        <div>
+          Icons made by <a href="http://www.flaticon.com/authors/dave-gandy"
+          title="Dave Gandy" target="_blank">Dave Gandy</a> from <a href="http://www.flaticon.com"
+          title="Flaticon" target="_blank">www.flaticon.com</a> is licensed by <a
+          href="http://creativecommons.org/licenses/by/3.0/"
+          title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
+        </div>
+      </div>
+    )
+  }
+};
