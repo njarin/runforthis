cdd66f6116c57cb2af18661296c6fa9478176a47
Dan McKeon <dpmckeon@gmail.com>
2017-07-07 11:08:25 -0700

    add disclaimer to footer

--- app/assets/javascripts/components/Footer.js.jsx
+++ app/assets/javascripts/components/Footer.js.jsx
@@ -3,7 +3,11 @@ class Footer extends React.Component {
   render() {
     return (
       <div className="footer">
-        <span><a href="/about">About Us</a></span> | <span>Find our project on <a href="https://github.com/njarin/runforthis" target="_blank">Github</a></span>
+        <span><a href="/about">About Us</a></span> | <span>Find our project on <a
+          href="https://github.com/njarin/runforthis" target="_blank">Github</a></span>
+        <p>Disclaimer: We have made our best effort to provide accurate data on this site,
+          but cannot guarantee the validity due to frequent special elections and other
+          factors that effect the timeliness of our data</p>
         <div>
           Icons made by <a href="http://www.flaticon.com/authors/dave-gandy"
           title="Dave Gandy" target="_blank">Dave Gandy</a> from <a href="http://www.flaticon.com"
