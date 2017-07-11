528a4d40e398c4788a47df5f6ce0c91777d9d815
Nicholas Jarin <nicholasjarin@gmail.com>
2017-07-11 16:11:35 -0700

    Style mission and add background image to body

--- app/assets/javascripts/components/Mission.js.jsx
+++ app/assets/javascripts/components/Mission.js.jsx
@@ -1,8 +1,8 @@
 class Mission extends React.Component {
   render() {
     return (
-      <div className="mission-component container">
-        <div className="mission-statement-wrapper">
+      <div className="mission-component">
+        <div className="mission-statement-wrapper container">
           <p className="mission-statement">
             You should run for office! No, really! Local elected officials have a significant impact on your day-to-day life. About 33% of state-level legislators run unopposed. That’s a problem if you believe in democracy. Enter your address below and see what local offices are opening up near you and how you can run for those seats.
           </p>
